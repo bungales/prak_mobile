830d4b1cc2d022b41778ef5b9a65a02fe194e156
bunga mobile <bunga24si@mahasiswa.pcr.ac.id>
2026-04-08 08:28:08 +0700

    Pertemuan 4

--- app/src/main/java/com/example/bungaapps/MainActivity.kt
+++ app/src/main/java/com/example/bungaapps/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.bungaapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bungaapps.Pertemuan_3.ThirdResultActivity
+import com.example.bungaapps.databinding.ActivityMainBinding
+import com.example.bungaapps.databinding.ActivityThirdBinding
+import com.example.bungaapps.pertemuan_4.FourthActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnToFourth.setOnClickListener {
+            intent.putExtra("nama", "Politeknik Caltex Riau")
+            intent.putExtra("asal", "Rumbai")
+            intent.putExtra("usia", 25)
+
+            startActivity(intent)
+            finish()
+
         }
     }
 }
---
+++ app/src/main/java/com/example/bungaapps/pertemuan_4/FourthActivity.kt
@@ -0,0 +1,45 @@
+package com.example.bungaapps.pertemuan_4
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bungaapps.R
+import com.example.bungaapps.databinding.ActivityFourthBinding
+import com.example.bungaapps.databinding.ActivityMainBinding
+
+class FourthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFourthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e("onCreate", "FourthActivity dibuat pertama kali")
+        enableEdgeToEdge()
+        binding = ActivityFourthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val name = intent.getStringExtra("nama")
+        val from = intent.getStringExtra("asal")
+        val age = intent.getIntExtra("usia",0)
+        Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+
+        binding.btnBack.setOnClickListener {
+            finish()
+ }
+
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.e("onStart", "onStart: {nama_activity} terlihat di layar")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("onDestroy", "{nama_activity} dihapus dari stack")
+    }
+}
